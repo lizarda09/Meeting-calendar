ea392e965effe28c1d7438e5b345f156efd01aae
Lizarda <mirliz3748@gmail.com>
2021-01-29 00:50:18 +0200

    added verification

--- create_page/js/create_functions.js
+++ create_page/js/create_functions.js
@@ -3,6 +3,15 @@ import {dayEvent, nameEvent, teamEvent, timeEvent} from "./create_constants.js";
 export function createNewEvent(e){
     e.preventDefault();
 
+    for(let key in localStorage) {
+        if(localStorage.hasOwnProperty(key)){
+            const item = JSON.parse(localStorage[key]);
+            if(dayEvent.value === item.day && timeEvent.value === item.time){
+                alert("You cannot add event on this date!");
+                return;
+            }
+        }
+    }
     const event = {
         id: new Date().getTime(),
         name: nameEvent.value,
@@ -12,4 +21,6 @@ export function createNewEvent(e){
     };
     localStorage.setItem(event.id, JSON.stringify(event));
     window.location.href = "../main_page/index.html";
+
+
 }
